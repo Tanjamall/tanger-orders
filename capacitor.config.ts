d33a149429aa9e74cf1ea058906a42c8891b2adc
Tanjamall <297609125+Tanjamall@users.noreply.github.com>
2026-08-22 00:08:08 +0100

    Add Capacitor Android project

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.tanjamall.tangerorders',
+  appName: 'Tanger Orders',
+  webDir: 'dist'
+};
+
+export default config;
